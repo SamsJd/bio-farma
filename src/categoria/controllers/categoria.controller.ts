8d9c42bda0664a3f3e0f60f0691ef1329365ce76
User <samarajeise@hotmail.com>
2025-07-08 13:18:39 -0300

    :construction: findAllByTitulo

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -24,4 +24,10 @@ export class CategoriaController {
   findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
     return this.categoriaService.findById(id);
   }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('titulo') titulo: string): Promise<Categoria[]> {
+    return this.categoriaService.findAllByTitulo(titulo);
+  }
 }
